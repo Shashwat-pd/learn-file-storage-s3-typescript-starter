830dc93f299fbe8a989934d33e5a9a013d8d7ae4
shashwat-pd <shashwatp07@gmail.com>
2025-11-15 09:53:47 +0545

    xx

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,11 @@
+import { join } from "path";
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { randomBytes } from "crypto";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -37,6 +39,7 @@ export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
 }
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
+  const MAX_UPLOAD_SIZE = 10 * 1024 * 1024;
   const { videoId } = req.params as { videoId?: string };
   if (!videoId) {
     throw new BadRequestError("Invalid video ID");
@@ -47,7 +50,34 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const image = formData.get("thumbnail");
 
-  return respondWithJSON(200, null);
+  if (!(image instanceof File)) {
+    throw new BadRequestError("Not an image");
+  }
+  if (image.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Too Large");
+  }
+
+
+  let file = await image.arrayBuffer();
+  let fileName = randomBytes(32).toString("base64");
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new BadRequestError("Video not found");
+  }
+  if (video.userID != userID) {
+    throw new UserForbiddenError("Not Authorized");
+  }
+  const fsPath = join(cfg.assetsRoot, `${fileName}.png`);
+
+  const url = `http://localhost:8091/${fsPath}`;
+  await Bun.write(fsPath, file, { createPath: true });
+  video.thumbnailURL = url;
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,61 @@
+import { join } from "path";
+import { randomUUID, randomBytes } from "crypto";
 import { respondWithJSON } from "./json";
-
+import { getBearerToken, validateJWT } from "../auth";
+import { UserForbiddenError, BadRequestError } from "./errors.ts";
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+import { getVideo, updateVideo } from "../db/videos.ts";
+import { s3, S3Client } from "bun";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const MAX_UPLOAD_SIZE = 10 * 1024 * 1024 * 1024;
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const video = getVideo(cfg.db, videoId);
+
+  if (!video) {
+    throw new BadRequestError("Video not found");
+  }
+  if (video.userID != userID) {
+    throw new UserForbiddenError("Not Authorized");
+  }
+
+  const formData = await req.formData();
+  const upload = formData.get("video");
+  if (!(upload instanceof File)) {
+    throw new BadRequestError("Not an image");
+  }
+
+  if (upload.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Too Large");
+  }
+  if (upload.type != "video/mp4") {
+    throw new BadRequestError("Invalid format");
+  }
+  let fileName = randomUUID() + ".mp4";
+  const fsPath = join(cfg.assetsRoot, `${fileName}.mp4`);
+
+  await Bun.write(fsPath, upload, { createPath: true });
+  const key = randomUUID() + ".mp4";
+  const bucketPath = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${key}`;
+  const file = Bun.file(fsPath);
+
+  const bucketFile = S3Client.file(key, {
+    ...cfg.s3Client,
+    type: file.type,
+  });
+  await bucketFile.write(file, { type: file.type });
+  video.videoURL = bucketPath;
+
+  updateVideo(cfg.db, video);
+  await file.delete();
+
   return respondWithJSON(200, null);
 }
